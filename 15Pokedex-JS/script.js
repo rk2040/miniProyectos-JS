3036d01fae216ebeb475a3b575c302832bd3e2c2
rk2040 <rpardo2040@gmail.com>
2023-06-03 20:39:35 -0300

    1er consulta API

--- 15Pokedex-JS/script.js
+++ 15Pokedex-JS/script.js
@@ -1,6 +1,54 @@
 
                         //'DOMContentLoaded' Espera que responda la API (y agrego un loader)
 document.addEventListener('DOMContentLoaded', ()=>{
+
+    const fetchPokemons = async (endpoint)=>{
+        let data;
+
+        try{
+            const response = await fetch(endpoint, {
+                method:'Get', 
+                headers:{
+                    'content-Type':'application/json'
+                }
+            })
+            data = await response.json();
+        }
+        catch(error){console.log(error)}
+
+        return data.pokemon_species;
+    };
+
+    function orderNumber(str){  //Obtengo el numero de pokemon Ej. pikachu: 25
+        let mySubtring = str.substring( str.lastIndexOf('s/') + 2, str.lastIndexOf('/') )
+
+        return mySubtring;
+    }
+
+    async function getPokemons(numero){
+        let endpoint = `https://pokeapi.co/api/v2/generation/${numero}/`
+
+        let container = document.getElementById('container');
+        container.innerHTML = '';
+
+        let pokemons = [];
+        pokemons = await fetchPokemons(endpoint);
+        for(let i=0; i<pokemons.length; i++){
+            pokemons[i].num = orderNumber(pokemons[i].url)
+        }
+
+        pokemons.sort( (a,b)=> a.num - b.num)
+        //console.log(pokemons);
+        pokemons.forEach((item) => {
+            let divItem = document.createElement('li');
+            divItem.classList.add('item');
+            divItem.innerHTML = `<div> ${orderNumber(item.url)} - ${item.name} </div>`;
+            container.appendChild(divItem);
+        });
+
+    }
+    getPokemons(1)
+
     let generacion = [
         "generation-1",
         "generation-2",
@@ -10,6 +58,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
         "generation-6",
         "generation-7"
     ];
+
 
     let filters = document.getElementById('filters');
     let gen = '';
